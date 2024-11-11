4779fec2cb98f30c6235a50a867c5954efabfdaf
kisahn new livewire <kishansaini>
2024-11-11 21:32:04 +0530

    data type

---
+++ datatype.js
@@ -0,0 +1,8 @@
+//console.log("kishan");
+let name = "kishan";
+let age = 26;
+let username = "kishan saini";
+let password = 12345678;
+// console.table([name, age, username, password]);
+// console.log(typeof name);
+console.table([name, age, username, password].map(item => typeof item));
