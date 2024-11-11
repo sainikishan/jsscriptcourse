409af5721552a2d5d4be08af080774d47359b450
kisahn new livewire <kishansaini>
2024-11-11 20:24:09 +0530

    first commit

---
+++ letvarconst.js
@@ -0,0 +1,6 @@
+const notchange = "kishan";
+let accountname = "kishansiani";
+let accountemail = "kishansaini666@gmial.com";
+var accountpassowrd = 12345678
+let username;
+console.table([notchange,accountname,accountemail,accountpassowrd,username]);
