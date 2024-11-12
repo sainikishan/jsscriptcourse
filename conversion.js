57a1f579531815c36abcbf17090d8d45743b9982
kisahn new livewire <kishansaini>
2024-11-12 19:55:40 +0530

    second commit

---
+++ conversion.js
@@ -0,0 +1,15 @@
+let score = 33;
+console.log(typeof score);
+console.log(typeof (score));
+let valueInnum = Number(score)
+console.log(typeof valueInnum);
+console.log(valueInnum);
+
+//33=>33
+// "33abc" => Nan
+// true = 1;false=>0
+//boolena conversion emplty string show false
+let isloggedin = "kishan"
+let booleanisloogedin = Boolean(isloggedin)
+console.log(booleanisloogedin);
+//endboolean
