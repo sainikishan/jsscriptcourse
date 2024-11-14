710c0637ec943a8ac53efbf337a3e2ce1fa74ba5
kishan saini <kishansaini666@gmail.com>
2024-11-14 19:48:15 +0530

    operational

---
+++ stackandheap.js
@@ -0,0 +1,20 @@
+
+//stack memory code(primitive)
+let name = "kishan";
+let newname = name;
+newname = "saini";
+console.log(name);
+console.log(newname);
+
+
+//heap(nonprimitve);
+let user = {
+    name:"ksiahn saini",
+    upi :"saini@333axis",
+}
+let usertwo = user;
+usertwo.email = "saini66@gmail.com";
+console.log(usertwo.email);
+console.log(user.email);
+
+
