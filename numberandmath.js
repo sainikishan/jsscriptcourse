3647445e127b88d23162a989161ebe8ad9ae86ab
kishan saini <kishansaini666@gmail.com>
2024-12-10 10:41:42 +0530

    instructions

--- numberandmath.js
+++ numberandmath.js
@@ -24,3 +24,13 @@ console.log(Math.abs(-4));
 console.log(Math.round(2.89));
 console.log(Math.ceil(4.2)); //this for top value
 console.log(Math.floor(4.9)); //this for lower value
+console.log(Math.min(4,3,6,8,9,2)); //this for lowesr value
+console.log(Math.max(4,5,7,2,3,4,5)); //this for greater value
+
+console.log(Math.random());//always value 0 and 1 this is always come on range
+console.log((Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+Math.random( )
