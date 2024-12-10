d73749f31c421db64f5d033f35b703ff67192d85
kishan saini <kishansaini666@gmail.com>
2024-12-10 20:18:18 +0530

    instructions

---
+++ 02array.js
@@ -0,0 +1,15 @@
+const myArr = [1, 2, 3, 4, 5]
+const myHeros = ["lkisa", "saijni"];
+console.log(myArr[3]);
+console.log(myHeros[0]);
+//array bsic end
+//array method
+//add in array
+myArr.push(6)
+console.log(myArr);
+//pop (remove in array)
+myArr.pop()
+console.log(myArr);
+//unshift  add value in array start
+myArr.unshift(9)
+console.log(myArr);
