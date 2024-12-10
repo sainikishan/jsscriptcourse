2da8d24b08056c88317ba06ee2a3569e15b72bc3
kishan saini <kishansaini666@gmail.com>
2024-12-10 19:46:31 +0530

    instructions

---
+++ dateandtime.js
@@ -0,0 +1,10 @@
+let date = new Date();
+console.log(date);
+console.log(date.toString());
+console.log(date.toDateString());
+console.log(date.toLocaleDateString());
+console.log(typeof date);
+
+let myCreatedDate = new Date(2023, 3, 23);
+console.log(myCreatedDate);
+console.log(myCreatedDate.toDateString());
